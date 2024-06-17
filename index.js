84105389f3434747069e16b035175c23e096ccb4
Anaïs PERIGNY <anaisperigny31@gmail.com>
2024-06-17 15:05:26 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 const jourNuit = document.getElementById('jourNuit');
 const body = document.getElementById('page');
 
+
+// MODE JOUR/NUIT
 jourNuit.addEventListener('click', () => {
     console.log("entrée")
     body.classList.toggle("darkMode");
 })
+const resultsContainer = document.querySelector("#results");
+const button = document.querySelector("#start");
+const btnScreenshot = document.querySelector("#screenshot");
+const emitterVideo = document.querySelector("#emitter-video");
+
+const snapshot = () => {
+  const canvas = document.createElement("canvas");
+  canvas
+    .getContext("2d")
+    .drawImage(emitterVideo, 0, 0, canvas.width, canvas.height);
+
+  resultsContainer.appendChild(canvas);
+};
+
+button.addEventListener("click", () => {
+  navigator.getUserMedia(
+    {
+      video: true,
+    },
+    (stream) => {
+      if ("srcObject" in emitterVideo) {
+        emitterVideo.srcObject = stream;
+      } else {
+        emitterVideo.src = window.URL.createObjectURL(stream);
+      }
+      emitterVideo.play();
+    },
+    () => {
+      alert("ERROR: camera or video not available");
+    }
+  );
+});
+
+btnScreenshot.addEventListener("click", () => {
+  snapshot();
+});
